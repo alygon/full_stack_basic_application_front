0a4d7bf7160784e7b09871abb03226d179fd41c1
Alysson Goncalves <alyssong@impa.br>
2023-05-03 04:25:53 -0300

    Atualizações FRONT

--- js/main.js
+++ js/main.js
@@ -1,161 +1,165 @@
 /*
-  --------------------------------------------------------------------------------------
-  Função para obter a lista existente do servidor via requisição GET
-  --------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------
+Função para obter a lista de usuários existentes do servidor 
+--------------------------------------------------------------------------------------
 */
 const getList = async () => {
-    let url = 'http://127.0.0.1:5000/usuarios';
-    fetch(url, {
-      method: 'get',
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        data.usuarios.forEach(item => insertList(item.login, item.nome))
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-  }
 
-/*
-    --------------------------------------------------------------------------------------
-    Chamada da função para carregamento inicial dos dados
-    --------------------------------------------------------------------------------------
-  */
-	getList()
-
-/*
-    --------------------------------------------------------------------------------------
-    Função para inserir registros na tabela mais abaixo na página
-    --------------------------------------------------------------------------------------
-*/
-	const insertList = (login, nome) => {
-		
-		var usuario = [login, nome]
-		var table = document.getElementById('myTable');
-		var row = table.insertRow();
-	  
-		for (var i = 0; i < usuario.length; i++) {
-		  var cel = row.insertCell(i);
-		  cel.textContent = usuario[i];
-		}
-		insertButtonExcluir(row.insertCell(-1))
-		removeElement()
-		insertButtonDetalhar(row.insertCell(-1))
-		getElement()
-	}
-
-/*
-    --------------------------------------------------------------------------------------
-    Função que cria botão para apagar registro da lista
-    --------------------------------------------------------------------------------------
-  */
-	const insertButtonExcluir = (parent) => {
-		let spanExcluir = document.createElement("span");
-		let imagemExcluir = document.createElement("img");
-		imagemExcluir.src = "https://cdn-icons-png.flaticon.com/512/126/126468.png";
-		imagemExcluir.height = "20";
-		imagemExcluir.width = "20";
-		spanExcluir.className = "close";
-		spanExcluir.appendChild(imagemExcluir);
-		parent.appendChild(spanExcluir);
-	  }
-
-/*
-    --------------------------------------------------------------------------------------
-    Função que cria botão para detalhar registro da lista
-    --------------------------------------------------------------------------------------
-  */
-	const insertButtonDetalhar = (parent) => {
-		let spanDetalhar = document.createElement("span");
-		let imagemDetalhar = document.createElement("img");
-		imagemDetalhar.src = "https://cdn-icons-png.flaticon.com/512/126/126474.png";
-		imagemDetalhar.height = "20";
-		imagemDetalhar.width = "20";
-		spanDetalhar.className = "detalhar";
-		spanDetalhar.appendChild(imagemDetalhar);
-		parent.appendChild(spanDetalhar);
-	  }
-
-/*
-    --------------------------------------------------------------------------------------
-    Função para remover registro da lista de acordo com o click no botão close
-    --------------------------------------------------------------------------------------
-  */
-	const removeElement = () => {
-		let close = document.getElementsByClassName("close");
-		let i;
-		for (i = 0; i < close.length; i++) {
-		  close[i].onclick = function () {
-			let div = this.parentElement.parentElement;
-			const nomeItem = div.getElementsByTagName('td')[0].innerHTML
-			if (confirm("Confirma Exclusão?")) {
-			  div.remove()
-			  deleteItem(nomeItem)
-			  alert("Dados Apagados!")
-			}
-		  }
-		}
-	  }
-
-/*
-    --------------------------------------------------------------------------------------
-    Função para deletar um registro da lista do servidor via requisição DELETE
-    --------------------------------------------------------------------------------------
-  */
-	const deleteItem = (item) => {
-		let url = 'http://127.0.0.1:5000/usuario?login=' + item;
-		fetch(url, {
-		  method: 'delete'
-		})
-		  .then((response) => response.json())
-		  .catch((error) => {
-			console.error('Error:', error);
-		  });
-	  }
-
-
-/*
-    --------------------------------------------------------------------------------------
-    Função para recuperar dados do usuário
-    --------------------------------------------------------------------------------------
-  */
-	const getElement = () => {
-		let detalhar = document.getElementsByClassName("detalhar");
-		let i;
-		for (i = 0; i < detalhar.length; i++) {
-		  detalhar[i].onclick = function () {
-			document.getElementById("divTabelaComentarios").style.display = "block";
-			let div = this.parentElement.parentElement;
-			const nomeItem = div.getElementsByTagName('td')[0].innerHTML
-			 detalharItem(nomeItem)
-		  }
-		}
-	  }
-
-/*
-	--------------------------------------------------------------------------------------
-	Função para obter a lista existente do servidor via requisição GET
-	--------------------------------------------------------------------------------------
-*/
-	const detalharItem = async (item) => {
-		let url = 'http://127.0.0.1:5000/usuario?login=' + item;
-		fetch(url, {
-			method: 'get',
-		})
+	let url = 'http://127.0.0.1:5000/usuarios';
+	fetch(url, {
+		method: 'get',
+	})
 		.then((response) => response.json())
 		.then((data) => {
-		    populaFormulario(data.comentarios)
+		data.usuarios.forEach(item => insertList(item.login, item.nome))
 		})
 		.catch((error) => {
 		console.error('Error:', error);
 		});
-	}
+}
 
 /*
-	--------------------------------------------------------------------------------------
-	Função para obter a lista existente do servidor via requisição GET
-	--------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------
+Chamada da função para carregamento inicial dos dados
+--------------------------------------------------------------------------------------
+*/
+getList()
+
+/*
+--------------------------------------------------------------------------------------
+Função para inserir registros na tabela principal da página
+--------------------------------------------------------------------------------------
+*/
+const insertList = (login, nome) => {
+	
+	var usuario = [login, nome]
+	var table = document.getElementById('myTable');
+	var row = table.insertRow();
+	
+	for (var i = 0; i < usuario.length; i++) {
+		var cel = row.insertCell(i);
+		cel.textContent = usuario[i];
+	}
+
+	insertButtonExcluir(row.insertCell(-1))
+	removeElement()
+	insertButtonDetalhar(row.insertCell(-1))
+	getElement()
+}
+
+/*
+--------------------------------------------------------------------------------------
+Função que cria botão para apagar registros da lista de usuários
+--------------------------------------------------------------------------------------
+*/
+const insertButtonExcluir = (parent) => {
+
+	let spanExcluir = document.createElement("span");
+	let imagemExcluir = document.createElement("img");
+	imagemExcluir.src = "https://cdn-icons-png.flaticon.com/512/126/126468.png";
+	imagemExcluir.height = "20";
+	imagemExcluir.width = "20";
+	spanExcluir.className = "close";
+	spanExcluir.appendChild(imagemExcluir);
+	parent.appendChild(spanExcluir);
+}
+
+/*
+--------------------------------------------------------------------------------------
+Função que cria botão para detalhar registros de usuários da lista
+--------------------------------------------------------------------------------------
+*/
+const insertButtonDetalhar = (parent) => {
+
+	let spanDetalhar = document.createElement("span");
+	let imagemDetalhar = document.createElement("img");
+	imagemDetalhar.src = "https://cdn-icons-png.flaticon.com/512/126/126474.png";
+	imagemDetalhar.height = "20";
+	imagemDetalhar.width = "20";
+	spanDetalhar.className = "detalhar";
+	spanDetalhar.appendChild(imagemDetalhar);
+	parent.appendChild(spanDetalhar);
+}
+
+/*
+--------------------------------------------------------------------------------------
+Função para remover registro da lista de acordo com o click no botão close
+--------------------------------------------------------------------------------------
+*/
+const removeElement = () => {
+	
+	let close = document.getElementsByClassName("close");
+	let i;
+	for (i = 0; i < close.length; i++) {
+		close[i].onclick = function () {
+		let div = this.parentElement.parentElement;
+		const nomeItem = div.getElementsByTagName('td')[0].innerHTML
+		if (confirm("Confirma Exclusão?")) {
+			div.remove()
+			deleteItem(nomeItem)
+			alert("Dados Apagados!")
+		}
+		}
+	}
+}
+
+/*
+--------------------------------------------------------------------------------------
+Função para deletar um registro da lista do servidor 
+--------------------------------------------------------------------------------------
+*/
+const deleteItem = (item) => {
+	let url = 'http://127.0.0.1:5000/usuario?login=' + item;
+	fetch(url, {
+		method: 'delete'
+	})
+		.then((response) => response.json())
+		.catch((error) => {
+		console.error('Error:', error);
+		});
+}
+
+/*
+--------------------------------------------------------------------------------------
+Função para recuperar dados do usuário
+--------------------------------------------------------------------------------------
+*/
+const getElement = () => {
+	let detalhar = document.getElementsByClassName("detalhar");
+	let i;
+	for (i = 0; i < detalhar.length; i++) {
+		detalhar[i].onclick = function () {
+		document.getElementById("divTabelaComentarios").style.display = "block";
+		let div = this.parentElement.parentElement;
+		const nomeItem = div.getElementsByTagName('td')[0].innerHTML
+			detalharItem(nomeItem)
+		}
+	}
+}
+
+/*
+--------------------------------------------------------------------------------------
+Função para obter a lista existente do servidor 
+--------------------------------------------------------------------------------------
+*/
+const detalharItem = async (item) => {
+	let url = 'http://127.0.0.1:5000/usuario?login=' + item;
+	fetch(url, {
+		method: 'get',
+	})
+	.then((response) => response.json())
+	.then((data) => {
+		populaFormulario(data.comentarios)
+	})
+	.catch((error) => {
+	console.error('Error:', error);
+	});
+}
+
+/*
+--------------------------------------------------------------------------------------
+Função para popular a popup com os comentários dos usuários
+--------------------------------------------------------------------------------------
 */
 const populaFormulario = (comentarios) => {
 
@@ -168,9 +172,9 @@ const populaFormulario = (comentarios) => {
 }
 
 /*
-  --------------------------------------------------------------------------------------
-  Função para colocar um comentário na lista do servidor via requisição POST
-  --------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------
+Função para colocar um comentário na lista do servidor 
+--------------------------------------------------------------------------------------
 */
 const inserirComentario = async (loginValor, descricaoValor) => {
 	const formData = new FormData();
@@ -186,8 +190,14 @@ const inserirComentario = async (loginValor, descricaoValor) => {
 	  .catch((error) => {
 		console.error('Error:', error);
 	  });
-  }
+}
 
+
+/*
+--------------------------------------------------------------------------------------
+Função principal, que recupera os dados do formulário e grava os dados no banco
+--------------------------------------------------------------------------------------
+*/
 $(function() {
 
 	var contactForm = function() {
@@ -237,8 +247,8 @@ $(function() {
 			} );
 		}
 	};
-	contactForm();
 
+	contactForm();
 });
 
 function openForm() {
@@ -255,14 +265,17 @@ function closeForm() {
 		document.getElementById("divTabelaComentarios").style.display = "none";
 }
 
- // Get the modal
- var modal = document.getElementById("divTabelaComentarios");
 
- // Get the <span> element that closes the modal
- var span = document.getElementsByClassName("fechar")[0];
+var modal = document.getElementById("divTabelaComentarios");
+
+var span = document.getElementsByClassName("fechar")[0];
  
- // When the user clicks on <span> (x), close the modal
- span.onclick = function() {
+/*
+--------------------------------------------------------------------------------------
+Função que fecha a modal com os comentários, excluindo registros da página
+--------------------------------------------------------------------------------------
+*/
+span.onclick = function() {
    
 	var linhas = document.getElementById("tabelaComentarios").rows;
 	var tabela = document.getElementById("tabelaComentarios");
@@ -270,10 +283,15 @@ function closeForm() {
 		tabela.deleteRow(1)
 	}
 	modal.style.display = "none";
- }
+}
  
- // When the user clicks anywhere outside of the modal, close it
- window.onclick = function(event) {
+
+/*
+--------------------------------------------------------------------------------------
+Função que fecha a modal com os comentários, excluindo registros da página
+--------------------------------------------------------------------------------------
+*/
+window.onclick = function(event) {
    if (event.target == modal) {
 	var linhas = document.getElementById("tabelaComentarios").rows;
 	var tabela = document.getElementById("tabelaComentarios");
@@ -282,4 +300,4 @@ function closeForm() {
 	}
 	 modal.style.display = "none";
    }
- }
+}
